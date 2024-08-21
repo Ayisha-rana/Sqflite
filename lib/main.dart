7f498e998f162e8ac1b91e2927cdefd396263d88
Ayisha <ayisharana137@gmail.com>
2024-08-21 11:16:23 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.teal,
+          backgroundColor: Colors.red,
         ),
       ),
     );
